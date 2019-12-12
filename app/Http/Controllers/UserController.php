34921a0f32327bd6d73a3f5d05598a7f57363ea5
Lorenzo <lorenzoperezga@gmail.com>
2019-12-12 16:56:26 +0100

    User y password codificiados con JWT

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Firebase\JWT\JWT;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function login(Request $r){
-        return $r->getContent();
+        $key = env('JWT_KEY');
+        $decode = JWT::decode($r->getContent(),$key, array('HS256'));
+        $decoded_array = (array) $decode;
+        return $decoded_array;
     }
 }
