d34ace15fe23a3ac7532fd0aaf60e9021aef7420
Lorenzo <lorenzoperezga@gmail.com>
2019-12-14 14:44:54 +0100

    A medio terminal

--- app/Http/Middleware/checkLogin.php
+++ app/Http/Middleware/checkLogin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Firebase\JWT\JWT;
+use App\User;
 class checkLogin
 {
     /**
@@ -16,9 +17,20 @@ class checkLogin
     public function handle($request, Closure $next)
     {
         $key = env('JWT_KEY');
-        $decode = JWT::decode($request->getContent(),$key, array('HS256'));
-        $decoded_array = (array) $decode;
-        $request->attributes->add(['login'=>$decoded_array]);
+        try{
+            $decode = JWT::decode($request->getContent(),$key, array('HS256'));
+            $decoded_array = (array) $decode;
+            //$request->attributes->add(['login'=>$decoded_array]);
+
+            $state = User::find($decoded_array['email']);
+            if(!emtpy($state)){
+                print_r($state);
+            }
+
+        }catch(\Exception $e){
+            return response($e->getMessage());
+        }
         return $next($request);
+
     }
 }
