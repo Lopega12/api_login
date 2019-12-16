4c6a63b02e2cea3b601de4d4389eaf9183e1f206
Lorenzo <lorenzoperezga@gmail.com>
2019-12-16 20:30:42 +0100

    Verificar si el user existe y la password es correcta

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,11 +5,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use App\User;
 class UserController extends Controller
 {
     public function login(Request $r){
-            Log::alert("el email: ". $r->get('login')['email']." ha entrado correctamente");
 
-        return "Inicio de sesion correcto";
+        if(!empty($r->get('login')) && $r->get('login') == true){
+            Log::alert("el email: ". $r->get('login')['email']." ha entrado correctamente");
+            $message= "Inicio de sesion correcto";
+        }else{
+            $message = '';
+        }
+
+        return $message;
     }
 }
--- app/Http/Middleware/checkLogin.php
+++ app/Http/Middleware/checkLogin.php
@@ -4,7 +4,9 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Firebase\JWT\JWT;
+
 use App\User;
+use Illuminate\Support\Facades\Log;
 class checkLogin
 {
     /**
@@ -20,16 +22,17 @@ class checkLogin
         try{
             $decode = JWT::decode($request->getContent(),$key, array('HS256'));
             $decoded_array = (array) $decode;
-            //$request->attributes->add(['login'=>$decoded_array]);
-
             $state = User::find($decoded_array['email']);
-            if(!emtpy($state)){
-                print_r($state);
-            }
+           if(!empty($state) && $state->password == $decoded_array['password']){
+            $request->attributes->add(['login'=>true]);
+            }else{
+               Log::alert("el email: ".$decoded_array['email']." ha intentado entrar");
+           }
 
         }catch(\Exception $e){
             return response($e->getMessage());
         }
+
         return $next($request);
 
     }
